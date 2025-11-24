b9984889a19fbc28592418029c6e5ce90d719a16
Matteo Carraro <mattycarraro01@gmail.com>
2025-11-24 22:11:12 +0100

    compito terminato, esercizi JS ancora incompleti

---
+++ script.js
@@ -0,0 +1,9 @@
+// esercizio 1
+function rimuovere() {
+    let twitter = document.querySelector('.blog-sidebar .p-4:nth-of-type(3) .list-unstyled li:nth-of-type(2)');
+    if (twitter) twitter.remove();
+    console.log(twitter);
+}
+rimuovere()
+
+// esercizio 2
